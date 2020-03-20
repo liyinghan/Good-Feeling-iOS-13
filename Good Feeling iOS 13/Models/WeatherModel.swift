441a4d6907eeaa37963cf6a52487ddb4c57912e5
jasonleehan@gmail.com <jasonleehan@gmail.com>
2020-03-20 21:13:32 +0800

    3/20. text and layout, plus can run on devices.but need to fix dark mode

--- Good Feeling iOS 13/Models/WeatherModel.swift
+++ Good Feeling iOS 13/Models/WeatherModel.swift
@@ -78,19 +78,19 @@ struct WeatherModel {
         switch indexInt {
         
         case -50 ... -1:
-            return "cold weather. Dress warm if practice outside activity"
+            return "天气寒冷，户外运动请注意保暖。"
         case 0 :
-            return "Well..let's take a day off. You should stop outside activity in practice"
+            return "休息一天。今天不适合户外运动。"
         case 1...10:
-            return "Listen to your body.If you feel good, go for it. If you don’t feel up to your regular workout or you feel sore and worn out, then you may need a lighter day."
+            return "留意身体给你的信号。如果感觉良好，那尽情训练吧。如果感到疲劳或者肌肉酸痛，那你今天需要休息调整一下。"
         case 11...25:
-            return "This is a good day for training. Optional water breaks approximately every 30 - 45 minutes for approximately 10 minutes duration."
+            return "非常适合运动的一天。训练30~45分钟后，适当补充水分。 "
         case 26...60:
-            return "Go for it and drink more water. Practice length should be 2 hours or less. Consider moving practice to morning or later in the day. Limited conditioning."
+            return "非常适合运动的一天。今天户外温度稍高，多喝水，训练时间不要超过2个小时。可以考虑将部分训练安排到早上或晚上。"
         case 61...200:
-            return "Try some indoor activities today.  Practice should not exceed 2-1/2 hours. Consider moving practice to morning or later in the day"
+            return "可以考虑进行室内训练。如果进行户外训练，务必多喝水，训练时间不要超过2个小时。可以考虑将部分训练安排到早上或晚上。"
         default:
-            return "You are ready for action today!"
+            return "开启运动的一天吧！"
         }
     } // end of indexName
     
